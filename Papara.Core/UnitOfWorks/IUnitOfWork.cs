27160863f898ef4945a9434947b52b0c6613abc1
kadiiravsaar <kadiiravsar@gmail.com>
2024-08-03 19:01:59 +0300

    Implement UnitOfWork pattern across repositories
    Added IUnitOfWork interface in the Core layer and implemented UnitOfWork class in the Repository layer to manage database transactions more effectively and ensure data integrity.

---
+++ Papara.Core/UnitOfWorks/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Papara.Core.UnitOfWorks
+{
+	public interface IUnitOfWork
+	{
+		Task CompleteAsync();	
+		Task CompleteWithTransaction();
+	}
+}
---
+++ Papara.Repository/UnitOfWorks/UnitOfWork.cs
@@ -0,0 +1,45 @@
+﻿using Papara.Core.UnitOfWorks;
+using Papara.Repository.Context;
+
+
+namespace Papara.Repository.UnitOfWorks
+{
+
+	public class UnitOfWork : IUnitOfWork, IDisposable
+	{
+		private readonly MsSqlDbContext _context;
+
+		public UnitOfWork(MsSqlDbContext context)
+		{
+			_context = context;
+		}
+
+		public void Dispose()
+		{
+			_context.Dispose();
+		}
+
+		public async Task CompleteAsync()
+		{
+			await _context.SaveChangesAsync();
+		}
+
+		public async Task CompleteWithTransaction()
+		{
+			using (var dbTransaction = await _context.Database.BeginTransactionAsync())
+			{
+				try
+				{
+					await _context.SaveChangesAsync();
+					await dbTransaction.CommitAsync();
+				}
+				catch (Exception ex)
+				{
+					await dbTransaction.RollbackAsync();
+					Console.WriteLine(ex);
+					throw;
+				}
+			}
+		}
+	}
+}
