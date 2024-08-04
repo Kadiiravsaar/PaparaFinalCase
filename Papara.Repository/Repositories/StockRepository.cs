c0c72675acd332fdb57a3d5cc574fed36dd27c8b
kadiiravsaar <kadiiravsar@gmail.com>
2024-08-04 21:21:27 +0300

    -Added new StockRepository.cs file. -Added new IStockRepository.cs file

---
+++ Papara.Repository/Repositories/StockRepository.cs
@@ -0,0 +1,16 @@
+﻿using Papara.Core.Models;
+using Papara.Core.Repositories;
+using Papara.Repository.Context;
+
+namespace Papara.Repository.Repositories
+{
+	public class StockRepository : GenericRepository<Stock>, IStockRepository
+	{
+		public StockRepository(MsSqlDbContext context) : base(context)
+		{
+		}
+	}
+
+
+	
+}
