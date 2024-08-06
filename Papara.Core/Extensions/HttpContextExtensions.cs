0a9035f04f2f72647aba5402512044543265f04e
kadiiravsaar <kadiiravsar@gmail.com>
2024-08-06 18:59:51 +0300

    Add CustomResponseDto and HttpContextExtensions for Improved API Responsiveness
    - Introduced CustomResponseDto to standardize API response structures.
    - Added HttpContextExtensions

---
+++ Papara.Core/Extensions/HttpContextExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Security.Claims;
+
+namespace Papara.Core.Extensions
+{
+	public static class HttpContextExtensions
+	{
+		public static string GetUserId(this HttpContext httpContext)
+		{
+			return httpContext.User?.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+		}
+	}
+}
