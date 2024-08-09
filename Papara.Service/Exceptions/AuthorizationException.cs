bf9605a9a7af38dde07d73b21b2a1bd83e9038ac
kadiiravsaar <kadiiravsar@gmail.com>
2024-08-09 23:42:30 +0300

    Added new exception classes for error handling
    - Added `AuthorizationException` for handling authorization errors.
    - Added `DeleteRestrictionException` for handling delete restrictions.

---
+++ Papara.Service/Exceptions/AuthorizationException.cs
@@ -0,0 +1,13 @@
+﻿namespace Papara.Service.Exceptions
+{
+	public class AuthorizationException : Exception
+	{	
+		public AuthorizationException(string message) : base(message)
+		{
+		}
+
+		public AuthorizationException(string message, Exception innerException) : base(message, innerException)
+		{
+		}
+	}
+}
---
+++ Papara.Service/Exceptions/DeleteRestrictionException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Papara.Service.Exceptions
+{
+	public class DeleteRestrictionException : Exception
+	{
+		public DeleteRestrictionException() : base() { }
+
+		public DeleteRestrictionException(string message) : base(message) { }
+
+		public DeleteRestrictionException(string message, Exception innerException) : base(message, innerException) { }
+	}
+}
